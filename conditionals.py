621eae5bd855a8a140eb5c7c9864cdf03ddb9f2a
urgentlion <urgentlioninfo@gmail.com>
2023-01-11 18:07:45 -0800

    initial commit

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+num_ = 21
+age_to_rent_car = 25
+
+if num_ >= age_to_rent_car:
+  print("You are old enough to rent a car")
+else:
+  gap_ = age_to_rent_car - num_
+  print(f"You must wait {gap_} more years. You are not old enough to rent a car")
+  
