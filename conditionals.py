fbca61c0baa5baab42f536a12b21872ebd67e411
urgentlion <urgentlioninfo@gmail.com>
2023-01-12 17:48:56 -0800

    ...

--- conditionals.py
+++ conditionals.py
@@ -7,3 +7,9 @@ else:
   gap_ = age_to_rent_car - num_
   print(f"You must wait {gap_} more years. You are not old enough to rent a car")
   
+age_ = int(input("How old are you? "))
+
+if age_ < 13:
+  print("You can't ride the roller coaster.")
+else:
+  print("You can ride the roller coaster!")
