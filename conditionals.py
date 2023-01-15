d327f7977e007ac834e242c1b71528e9d6bed924
urgentlion <urgentlioninfo@gmail.com>
2023-01-14 18:49:08 -0800

    ...

--- conditionals.py
+++ conditionals.py
@@ -13,3 +13,14 @@ if age_ < 13:
   print("You can't ride the roller coaster.")
 else:
   print("You can ride the roller coaster!")
+
+language_choice = input("What language do you speak? ")
+
+if language_choice == "English":
+  print("Hello!")
+elif language_choice == "Spanish":
+  print("Hola!")
+elif language_choice == "German":
+  print("Hallo!")
+else:
+  print("I don't know that language.")
